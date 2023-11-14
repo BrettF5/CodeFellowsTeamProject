fa61ed8c4159372d5ad4784576606a280eebdda5
Larry Latorre <llatorre481@gmail.com>
2023-11-14 13:16:57 -0600

    added button

--- js/app.js
+++ js/app.js
@@ -62,14 +62,18 @@ const form = document.getElementById('addCigarForm');
 const cigarList = document.getElementById('cigar-list');
 //Handles submit button
 function handleSubmit(event){
-	event.preventDefault();
-    //checks for correct inputs TODO
-
-
-    
+	event.preventDefault();  
+   
 	//stores values from form
 	cigarName = document.getElementById('cigar-name').value;
 	cigarQuantity = document.getElementById('cigar-quantity').value;
+    //checks for correct inputs TODO
+    if (cigarName === '' || cigarQuantity === ''){
+        alert('need an input');
+        return
+      }
+    //changes number string to an integer
+    let cigarQuantityInt = parseInt(cigarQuantity);
 	//creates list element
 	const cigarItem = document.createElement('li');
 	cigarItem.className = 'cigar-Item';
@@ -78,22 +82,26 @@ function handleSubmit(event){
 	cigarDetails.className = 'cigar-details';
 	cigarDetails.innerHTML = `<span class="name">Name: ${cigarName}</span>  <span>Quantity: ${cigarQuantity}</span>
     <button class="delete-button">delete</button>
-    <button class="button" onclick="increase">+</button>
-    <button class="button" onclick="decrease">-</button>`
+    <button id="buttonPlus" onclick="increase">+</button>
+    <button id="button-minus" onclick="decrease">-</button>`
     //appends elements
 	cigarItem.appendChild(cigarDetails);
 	cigarList.appendChild(cigarItem);
-	initCigar(cigarName, cigarQuantity);	event.target.reset();
+	initCigar(cigarName, cigarQuantity);	
+    event.target.reset();
+    
 }
 // Function to increase quantity
 function increaseQuantity() {
-
+    cigarQuantityInt += 1;
+    cigarDetails.innerHTML = `<span class="name">Name: ${cigarName}</span>  <span>Quantity: ${cigarQuantityInt}</span>`
 }
 
 // Function to decrease quantity
 function decreaseQuantity() {
-   
+    cigarQuantityInt -= 1;
 }
+
 
 function handleDelete(event) {
 	if(event.target.classList.contains('delete-button')){
@@ -101,6 +109,7 @@ function handleDelete(event) {
 		cigarList.removeChild(cigarItem);
 	}
 }
+buttonPlus.addEventListener('click',increaseQuantity)
 cigarList.addEventListener('click', handleDelete);
 form.addEventListener('submit', handleSubmit);
 
