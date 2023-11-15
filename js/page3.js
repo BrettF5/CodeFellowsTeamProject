f06044a18914b1c80647fd61691dda813b429ec1
BrettF5 <brettfort@hotmail.com>
2023-11-15 13:15:38 -0800

    adding third choice for bold option, fix few issue

--- js/page3.js
+++ js/page3.js
@@ -4,30 +4,31 @@ const askUserQuestions = []; // Array to store AskQuestion objects
 let q1 = null; // Variable to store the current AskQuestion object
 let countClick = 0;
 const maxClick = 3;
-let mildCount = 0;
-let mediumCount = 0;
 let leftProduct = 0;
 let centerProduct = 0;
+let rightProduct = 0;
 
 // Your HTML elements
 const questionAskID = document.getElementById('question');
 const leftImg = document.getElementById('img1');
 const centerImg = document.getElementById('img2');
+const rightImg = document.getElementById('img3');
 const returnCigarUser = document.getElementById('returnCigar');
 
 // Constructor function for AskQuestion objects
-function AskQuestion(question, img1, img2) {
+function AskQuestion(question, img1, img2, img3) {
     this.question = question;
     this.img1 = img1;
     this.img2 = img2;
+    this.img3 = img3; // Fix: Corrected the property name
 }
 
 // Sample AskQuestion instances
-const questionOne = new AskQuestion('Which Food do you prefer?', 'https://placehold.co/200x300?text=mild+food', 'https://placehold.co/200x300?text=medium+food');
+const questionOne = new AskQuestion('Which Food do you prefer?', 'https://placehold.co/200x300?text=mild+food', 'https://placehold.co/200x300?text=medium+food', 'https://placehold.co/200x300?text=bold+food');
 askUserQuestions.push(questionOne);
-const questionTwo = new AskQuestion('Which whisky do you prefer?', 'https://placehold.co/200x300?text=mild+whiskey', 'https://placehold.co/200x300?text=medium+whiskey');
+const questionTwo = new AskQuestion('Which whisky do you prefer?', 'https://placehold.co/200x300?text=mild+whiskey', 'https://placehold.co/200x300?text=medium+whiskey', 'https://placehold.co/200x300?text=bold+whiskey');
 askUserQuestions.push(questionTwo);
-const questionThree = new AskQuestion('Which beer do you prefer?', 'https://placehold.co/200x300?text=mild+beer', 'https://placehold.co/200x300?text=medium+beer');
+const questionThree = new AskQuestion('Which beer do you prefer?', 'https://placehold.co/200x300?text=mild+beer', 'https://placehold.co/200x300?text=medium+beer', 'https://placehold.co/200x300?text=bold+beer');
 askUserQuestions.push(questionThree);
 
 function renderQuestion() {
@@ -41,9 +42,10 @@ function renderQuestion() {
     // Display the question text in the questionAskID element
     questionAskID.textContent = q1.question;
 
-    // Set the src attribute for the left and center images
+    // Set the src attribute for the left, center, and right images
     leftImg.setAttribute('src', q1.img1);
     centerImg.setAttribute('src', q1.img2);
+    rightImg.setAttribute('src', q1.img3);
 
     countClick += 1;
 }
@@ -51,6 +53,7 @@ function renderQuestion() {
 function initEventListener() {
     leftImg.addEventListener('click', handleLeftProductClick);
     centerImg.addEventListener('click', handleCenterProductClick);
+    rightImg.addEventListener('click', handleRightProductClick);
 }
 
 function handleLeftProductClick() {
@@ -65,22 +68,30 @@ function handleCenterProductClick() {
     renderQuestion();
 }
 
+function handleRightProductClick() {
+    // Handle the click on the right image
+    rightProduct += 1;
+    renderQuestion();
+}
+
 function endClick() {
     // Remove event listeners when maxClick is reached
     leftImg.removeEventListener('click', handleLeftProductClick);
     centerImg.removeEventListener('click', handleCenterProductClick);
+    rightImg.removeEventListener('click', handleRightProductClick);
     returnCigarType(); // Render the cigar type to user
 }
 
 function returnCigarType() {
-    if (leftProduct > centerProduct) {
+    if (leftProduct > centerProduct && leftProduct > rightProduct) {
         returnCigarUser.textContent = 'I recommend a mild cigar';
+    } else if (centerProduct > leftProduct && centerProduct > rightProduct) {
+        returnCigarUser.textContent = 'I recommend a medium cigar';
     } else {
-        returnCigarUser.textContent ='I recommend a medium cigar';
+        returnCigarUser.textContent = 'I recommend a bold cigar';
     }
 }
 
 // Start App
 initEventListener(); // Initialize event listeners
 renderQuestion(); // Render the initial question
-
