ca13115ad1ed9ca7dfb2961df5214cbfa1d10101
BrettF5 <brettfort@hotmail.com>
2023-11-15 16:18:39 -0800

    adding third set of images for page3

--- js/page3.js
+++ js/page3.js
@@ -28,7 +28,7 @@ const questionOne = new AskQuestion('Which Food do you prefer?', 'img/cigars/mil
 askUserQuestions.push(questionOne);
 const questionTwo = new AskQuestion('Which whisky do you prefer?', 'img/cigars/FortyCreekMild.jpg', 'img/cigars/JamersonIrishMid.jpg', 'img/cigars/JohnnieWalkerBold.jpg');
 askUserQuestions.push(questionTwo);
-const questionThree = new AskQuestion('Which beer do you prefer?', 'https://placehold.co/200x300?text=mild+beer', 'https://placehold.co/200x300?text=medium+beer', 'https://placehold.co/200x300?text=bold+beer');
+const questionThree = new AskQuestion('Which beer do you prefer?', 'img/cigars/MildBeer.jpg', 'img/cigars/MidBeer.jpg', 'img/cigars/BoldBeer.jpg');
 askUserQuestions.push(questionThree);
 
 function renderQuestion() {
